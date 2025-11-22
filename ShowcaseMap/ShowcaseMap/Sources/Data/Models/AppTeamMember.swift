079e790b391a679751663e89e84d9cc4d833dd89
finn <bishoe01@gmail.com>
2025-11-22 15:53:34 +0900

    chore: 팀원의 누락된 링크드인 링크 추가

--- ShowcaseMap/ShowcaseMap/Sources/Data/Models/AppTeamMember.swift
+++ ShowcaseMap/ShowcaseMap/Sources/Data/Models/AppTeamMember.swift
@@ -26,7 +26,7 @@ struct AppTeamMember: Identifiable, Hashable {
 extension AppTeamMember {
     static let tntMembers: [AppTeamMember] = [
         AppTeamMember(id: "Go", name: "고재현 (GO)", position: "Product Owner", description: "치열하게 설계하고 집요하게 검증하며 앞으로만 갑니다.", linkedInURL: URL(string: "...")!),
-        AppTeamMember(id: "Elena", name: "김지윤 (Elena)", position: "Product Designer", description: "‘사람'을 위한 프로덕트를 디자인합니다.", linkedInURL: URL(string: "...")!),
+        AppTeamMember(id: "Elena", name: "김지윤 (Elena)", position: "Product Designer", description: "‘사람'을 위한 프로덕트를 디자인합니다.", linkedInURL: URL(string: "https://www.linkedin.com/in/jiyoon-kim-565951249/")!),
         AppTeamMember(id: "Air", name: "양시준 (Air)", position: "iOS Developer", description: "사용자 경험을 높이는 iOS 개발을 고민하고 만들어갑니다.", linkedInURL: URL(string: "https://www.linkedin.com/in/yangsijun/")!),
         AppTeamMember(id: "Finn", name: "정종문 (Finn)", position: "iOS Developer", description: "소통이 잘되는 개발자가 되기 위해 노력합니다.", linkedInURL: URL(string: "https://www.linkedin.com/in/jongmun-j-366673277/")!),
         AppTeamMember(id: "Dean", name: "정송헌 (Dean)", position: "iOS Developer", description: "테크 멘토가 되기 위해 목적과 근거를 가진 개발을 합니다", linkedInURL: URL(string: "https://www.linkedin.com/in/%EC%86%A1%ED%97%8C-%EC%A0%95-04a44b331/")!),
